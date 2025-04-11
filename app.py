23c45b6223cfb37d345a067334c81636ab2cb95a
sleenger <trilochansingh590@gmail.com>
2025-04-11 13:43:30 +0530

    app.py

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/proxy', methods=['GET'])
 @app.route('/')
 def home():
-    return "🚀 Flask Proxy Server is Running!"
+    return '🚀 Flask Proxy Server is Running!'
 
+@app.route('/proxy', methods=['GET'])
 def proxy():
     url = request.args.get('url')
     if not url:
         return jsonify({"error": "URL parameter is required"}), 400
 
     headers = {
-        "Authorization": f"5b3ce3597851110001cf6248b0d2d44302c042159f34a1ef0a4dd629"
+        "Authorization": "5b3ce3597851110001cf6248b0d2d44302c042159f34a1ef0a4dd629"
     }
 
     response = requests.get(url, headers=headers)
