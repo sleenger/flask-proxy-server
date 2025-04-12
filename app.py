44d6b66592c73b82d76ea8e6f0163b8f192d4439
sleenger <trilochansingh590@gmail.com>
2025-04-12 17:33:31 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import requests
 
 app = Flask(__name__)
@@ -12,7 +13,11 @@ def proxy():
     url = request.args.get('url')
     if not url:
         return jsonify({"error": "URL parameter is required"}), 400
-
+        
+@app.route('/test', methods=['POST'])
+def test():
+    data = request.get_json()
+    return jsonify({"status": "success", "received": data}), 200
     headers = {
         "Authorization": "5b3ce3597851110001cf6248b0d2d44302c042159f34a1ef0a4dd629"
     }
@@ -22,4 +27,4 @@ def proxy():
     return jsonify(response.json())
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
